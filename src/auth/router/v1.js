2ec34687ac297fc6636b0506fdb92d3b8ce6b0fe
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2022-01-20 08:56:58 -0600

    adds try catch blocks on all routes

--- src/auth/router/v1.js
+++ src/auth/router/v1.js
@@ -23,36 +23,56 @@ router.post('/:model', handleCreate);
 router.put('/:model/:id', handleUpdate);
 router.delete('/:model/:id', handleDelete);
 
-async function handleGetAll(req, res) {
-  let allRecords = await req.model.get();
-  res.status(200).json(allRecords);
+async function handleGetAll(req, res, next) {
+  try {
+    let allRecords = await req.model.get();
+    res.status(200).json(allRecords);
+  } catch (e) {
+    next(e.message);
+  }
 }
 
-async function handleGetOne(req, res) {
-  const id = req.params.id;
-  console.log(id);
-  console.log(req.model);
-  let theRecord = await req.model.get(id);
-  res.status(200).json(theRecord);
+async function handleGetOne(req, res, next) {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    console.log(req.model);
+    let theRecord = await req.model.get(id);
+    res.status(200).json(theRecord);
+  } catch (e) {
+    next(e.message);
+  }
 }
 
-async function handleCreate(req, res) {
-  let obj = req.body;
-  let newRecord = await req.model.create(obj);
-  res.status(201).json(newRecord);
+async function handleCreate(req, res, next) {
+  try {
+    let obj = req.body;
+    let newRecord = await req.model.create(obj);
+    res.status(201).json(newRecord);
+  } catch (e) {
+    next(e.message);
+  }
 }
 
-async function handleUpdate(req, res) {
-  const id = req.params.id;
-  const obj = req.body;
-  let updatedRecord = await req.model.update(id, obj);
-  res.status(200).json(updatedRecord);
+async function handleUpdate(req, res, next) {
+  try {
+    const id = req.params.id;
+    const obj = req.body;
+    let updatedRecord = await req.model.update(id, obj);
+    res.status(200).json(updatedRecord);
+  } catch (e) {
+    next(e.message);
+  }
 }
 
-async function handleDelete(req, res) {
-  let id = req.params.id;
-  let deletedRecord = await req.model.delete(id);
-  res.status(200).json(deletedRecord);
+async function handleDelete(req, res, next) {
+  try {
+    let id = req.params.id;
+    let deletedRecord = await req.model.delete(id);
+    res.status(200).json(deletedRecord);
+  } catch (e) {
+    next(e.message);
+  }
 }
 
 
